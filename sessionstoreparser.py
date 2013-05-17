54b719fa458429643c96d1a8ea24022c81e7b0db
Lesmana Zimmer <lesmana@gmx.de>
2013-05-17 23:11:57 +0200

    implement print open urls

--- sessionstoreparser.py
+++ sessionstoreparser.py
@@ -1,4 +1,22 @@
 #! /usr/bin/env python
 
+import json
+import sys
+
+def printopenurls(sessionstore):
+  for windows in sessionstore['windows']:
+    for tab in windows['tabs']:
+      openindex = tab['index'] - 1
+      openentry = tab['entries'][openindex]
+      openurl = openentry['url']
+      print openurl
+
 def main():
-  print 'main'
+  if len(sys.argv) != 2:
+    print 'need filename'
+    sys.exit(1)
+  filename = sys.argv[1]
+  fileob = open(filename)
+  sessionstore = json.load(fileob)
+  fileob.close()
+  printopenurls(sessionstore)
