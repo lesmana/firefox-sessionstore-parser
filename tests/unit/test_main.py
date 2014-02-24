005a4187d02e94a9cfbe9de6eec96b3284678d4b
Lesmana Zimmer <lesmana@gmx.de>
2014-02-24 01:49:16 +0100

    test noerror

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -7,6 +7,19 @@ import sessionstoreparser as p
 
 class TestMain(unittest.TestCase):
 
+  def test_noerror(self):
+    report = []
+    class FakeMain(object):
+      def trymain(self, argv):
+        report.append(('trymain', argv))
+    fakemain = FakeMain()
+    argv = ['wat']
+    exitstatus, errormessage = p.Main.main.__func__(fakemain, argv)
+    self.assertEqual(errormessage, None)
+    self.assertEqual(exitstatus, 0)
+    self.assertEqual(report, [
+          ('trymain', ['wat'])])
+
   def test_error(self):
     report = []
     class FakeMain(object):
