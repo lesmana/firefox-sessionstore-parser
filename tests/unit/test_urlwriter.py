2986ceeb8cc086866e233c8c58d1e984613dd33f
Lesmana Zimmer <lesmana@gmx.de>
2014-02-25 12:02:17 +0100

    remove unnecessary report

--- tests/unit/test_urlwriter.py
+++ tests/unit/test_urlwriter.py
@@ -8,14 +8,12 @@ import sessionstoreparser as p
 class TestWrite(unittest.TestCase):
 
   def test_empty(self):
-    report = []
     stringio = StringIO.StringIO()
     urlwriter = p.UrlWriter(stringio)
     urlwriter.write([])
     self.assertEqual(stringio.getvalue(), '')
 
   def test_someurls(self):
-    report = []
     stringio = StringIO.StringIO()
     urlwriter = p.UrlWriter(stringio)
     urlwriter.write(['url1', 'url2'])
