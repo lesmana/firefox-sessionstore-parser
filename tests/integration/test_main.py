cc45bf44a5312a3fbb0b63e79e1735e882e87423
Lesmana Zimmer <lesmana@gmx.de>
2014-03-05 20:28:13 +0100

    add integration test

---
+++ tests/integration/test_main.py
@@ -0,0 +1,35 @@
+
+import unittest
+
+import contextlib
+import StringIO
+import textwrap
+
+import sessionstoreparser as p
+
+class TestMain(unittest.TestCase):
+
+  def test_default(self):
+    fakefilecontent = textwrap.dedent('''\
+          {
+            "windows": [{
+              "selected": 1,
+              "tabs": [{
+                "index": 1,
+                "entries": [{
+                  "url": "http://window1tab1url1"}]}],
+              "_closedTabs": []}],
+            "_closedWindows": []}''')
+    fakefile = StringIO.StringIO(fakefilecontent)
+    def fakeopen(filename):
+      if filename == 'filename':
+        return contextlib.closing(fakefile)
+      else:
+        self.fail('unexpected filename: ' + filename)
+    fakestdout = StringIO.StringIO()
+    fakestderr = StringIO.StringIO()
+    fakeargv = ['progname', 'filename']
+    exitstatus = p.mainsecluded(fakeopen, fakestdout, fakestderr, fakeargv)
+    self.assertEqual(exitstatus, 0)
+    self.assertEqual(fakestdout.getvalue(), 'http://window1tab1url1\n')
+    self.assertEqual(fakestderr.getvalue(), '')
--- tests/rununittests.py
+++ tests/rununittests.py
@@ -3,6 +3,7 @@
 import unittestchooser
 
 import unit
+import integration
 
 if __name__ == '__main__':
   unittestchooser.main()
