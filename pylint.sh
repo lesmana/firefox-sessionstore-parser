0fca2cca0da9c7134343a1bc0a6a5fd5b86cc3ad
Lesmana Zimmer <lesmana@gmx.de>
2014-03-18 20:15:21 +0100

    add pylint runner

---
+++ pylint.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+pylint \
+  --rcfile=/dev/null \
+  --persistent=n \
+  --disable=locally-disabled \
+  --disable=missing-docstring \
+  --disable=no-self-use \
+  --disable=duplicate-code \
+  --disable=too-few-public-methods \
+  --disable=too-many-public-methods \
+  --disable=star-args \
+  --output-format=colorized \
+  --include-ids=y \
+  --symbols=y \
+  --reports=n \
+  --indent-string='  ' \
+  sessionstoreparser \
+  pytests
