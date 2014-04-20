764e19be0db3bedbb60765030324d67daa24d360
Lesmana Zimmer <lesmana@gmx.de>
2014-04-20 17:42:02 +0200

    cosmetic: rename to remove test_ prefix

--- tests_python/integration/__init__.py
+++ tests_python/integration/__init__.py
@@ -1,2 +1,2 @@
 
-from . import test_main
+from . import main
--- tests_python/unit/__init__.py
+++ tests_python/unit/__init__.py
@@ -1,6 +1,6 @@
 
-from . import test_argvhandler
-from . import test_jsonreader
-from . import test_urlgeneratorfactory
-from . import test_urlwriter
-from . import test_application
+from . import argvhandler
+from . import jsonreader
+from . import urlgeneratorfactory
+from . import urlwriter
+from . import application
