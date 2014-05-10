7b8b46a11c13c861b7c0d37375215d9c931fe800
Lesmana Zimmer <lesmana@gmx.de>
2014-05-10 17:52:14 +0200

    simplify coverage runner

--- coverage.sh
+++ coverage.sh
@@ -2,18 +2,8 @@
 
 export COVERAGE_FILE=coveragedatafile
 
-clean() {
-  rm -f $COVERAGE_FILE
-}
-
-trap clean EXIT
-
 coverage run --branch --omit="helpers/*,*/yaml/*" runpythontests.py || exit 1
 
 coverage report --show-missing
 
-if [ "$1" = "html" ]; then
-  coverage html
-  echo "coverage html done"
-  echo "point browser to htmlcov/index.html"
-fi
+coverage html --directory=coveragehtmlreport
