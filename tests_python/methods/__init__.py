c5d844376baf7167bc01d5799019a10ef471e3bb
Lesmana Zimmer <lesmana@gmx.de>
2014-05-20 22:24:52 +0200

    add sessionstoreparsertest

--- tests_python/methods/__init__.py
+++ tests_python/methods/__init__.py
@@ -3,4 +3,5 @@ from . import argvhandler
 from . import jsonreader
 from . import urlgeneratorfactory
 from . import urlwriter
+from . import sessionstoreparsertest
 from . import application
---
+++ tests_python/methods/sessionstoreparsertest.py
@@ -0,0 +1,9 @@
+
+# this file is named sessionstoreparsertest to avoid name conflict
+
+import unittest
+
+import StringIO
+
+import sessionstoreparser as p
+
