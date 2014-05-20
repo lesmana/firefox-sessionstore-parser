eb9b4a0cdfd6f73d9d23fb927c14bd227ed56099
Lesmana Zimmer <lesmana@gmx.de>
2014-05-20 22:25:34 +0200

    move relevant tests to sessionstoreparsertest

--- tests_python/methods/application.py
+++ tests_python/methods/application.py
@@ -19,60 +19,6 @@ class TestHandleArgv(unittest.TestCase):
     self.assertEqual(report, [
           ('handle', ['argv'])])
 
-class TestGetSessionStore(unittest.TestCase):
-
-  def test_noerror(self):
-    report = []
-    class FakeJsonReader(object):
-      def read(self, filename):
-        report.append(('read', filename))
-        return 'jsonobject'
-    app = p.Application(None, FakeJsonReader(), None, None)
-    sessionstore = app.getsessionstore({'filename': 'filename'})
-    self.assertEqual(sessionstore, 'jsonobject')
-    self.assertEqual(report, [
-          ('read', 'filename')])
-
-class TestGetUrlGenerator(unittest.TestCase):
-
-  def test_noerror(self):
-    report = []
-    class FakeUrlGeneratorFactory(object):
-      def produce(self):
-        report.append(('produce', ))
-        return 'urlgenerator'
-    app = p.Application(None, None, FakeUrlGeneratorFactory(), None)
-    urlgenerator = app.geturlgenerator()
-    self.assertEqual(urlgenerator, 'urlgenerator')
-    self.assertEqual(report, [
-          ('produce', )])
-
-class TestGetUrls(unittest.TestCase):
-
-  def test_noerror(self):
-    report = []
-    class FakeUrlGenerator(object):
-      def generate(self, sessionstore):
-        report.append(('generate', sessionstore))
-        return 'urls'
-    app = p.Application(None, None, None, None)
-    urls = app.geturls(FakeUrlGenerator(), 'sessionstore')
-    self.assertEqual(urls, 'urls')
-    self.assertEqual(report, [
-          ('generate', 'sessionstore')])
-
-class TestWriteUrls(unittest.TestCase):
-
-  def test_noerror(self):
-    report = []
-    class FakeUrlWriter(object):
-      def write(self, urls):
-        report.append(('write', urls))
-    app = p.Application(None, None, None, FakeUrlWriter())
-    app.writeurls('urls')
-    self.assertEqual(report, [
-          ('write', 'urls')])
-
 class TestTryRun(unittest.TestCase):
 
   def test_noerror(self):
