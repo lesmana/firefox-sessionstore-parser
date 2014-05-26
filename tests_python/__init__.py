70b6a8605c60c954c9bc94fb9d510dd8e43cbff1
Lesmana Zimmer <lesmana@gmx.de>
2014-05-26 15:19:07 +0200

    move tests up from methods

--- tests_python/__init__.py
+++ tests_python/__init__.py
@@ -1,3 +1,9 @@
 
-from . import methods
+from . import application
+
+from . import argvhandler
+from . import jsonreader
+from . import urlgeneratorfactory
+from . import urlwriter
+from . import sessionstoreparsertest
 from . import application
--- tests_python/methods/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from . import argvhandler
-from . import jsonreader
-from . import urlgeneratorfactory
-from . import urlwriter
-from . import sessionstoreparsertest
-from . import application
