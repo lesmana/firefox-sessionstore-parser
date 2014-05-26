0a0e5f8d000cb5c0262c6646cfdce29dcb7d34cb
Lesmana Zimmer <lesmana@gmx.de>
2014-05-26 15:21:34 +0200

    move tests up from application

--- tests_python/__init__.py
+++ tests_python/__init__.py
@@ -1,9 +1,8 @@
 
-from . import application
-
 from . import argvhandler
 from . import jsonreader
 from . import urlgeneratorfactory
 from . import urlwriter
 from . import sessionstoreparsertest
 from . import application
+from . import main
