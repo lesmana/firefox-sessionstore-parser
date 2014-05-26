b3aadfdb8bba7f47ff6c8facb9ccaeca7947c0e0
Lesmana Zimmer <lesmana@gmx.de>
2014-05-26 14:48:09 +0200

    merge argvhandler tests together

--- tests_python/methods/argvhandler.py
+++ tests_python/methods/argvhandler.py
@@ -86,3 +86,54 @@ class TestHandle(unittest.TestCase):
       self.fail('expected exception') # pragma: no cover
     self.assertEqual(report, [
           ('getopt', 'argv')])
+
+class TestArgvHandler(unittest.TestCase):
+
+  def test_foo(self):
+    shortopts = 'hf:b'
+    longopts = ['help', 'foo=', 'bar']
+    optnametable = {
+          '-h': 'help',
+          '--help': 'help',
+          '-f': 'foo',
+          '--foo': 'foo',
+          '-b': 'bar',
+          '--bar': 'bar'}
+    argvhandler = p.ArgvHandler(shortopts, longopts, optnametable)
+    options = argvhandler.handle(['-f', 'somefoo', 'filename'])
+    self.assertEqual(options, {
+          'foo': 'somefoo',
+          'filename': 'filename'})
+
+  def test_bar(self):
+    shortopts = 'hf:b'
+    longopts = ['help', 'foo=', 'bar']
+    optnametable = {
+          '-h': 'help',
+          '--help': 'help',
+          '-f': 'foo',
+          '--foo': 'foo',
+          '-b': 'bar',
+          '--bar': 'bar'}
+    argvhandler = p.ArgvHandler(shortopts, longopts, optnametable)
+    options = argvhandler.handle(['--bar', 'filename'])
+    self.assertEqual(options, {
+          'bar': True,
+          'filename': 'filename'})
+
+  def test_foobar(self):
+    shortopts = 'hf:b'
+    longopts = ['help', 'foo=', 'bar']
+    optnametable = {
+          '-h': 'help',
+          '--help': 'help',
+          '-f': 'foo',
+          '--foo': 'foo',
+          '-b': 'bar',
+          '--bar': 'bar'}
+    argvhandler = p.ArgvHandler(shortopts, longopts, optnametable)
+    options = argvhandler.handle(['--bar', '--foo', 'somefoo', 'filename'])
+    self.assertEqual(options, {
+          'bar': True,
+          'foo': 'somefoo',
+          'filename': 'filename'})
