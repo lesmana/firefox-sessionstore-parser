4fb60417fd150be07bfebd8141b4b78b6b04f83c
Lesmana Zimmer <lesmana@gmx.de>
2014-06-03 08:46:47 +0200

    test literals with correct structure

--- tests_python/application.py
+++ tests_python/application.py
@@ -11,24 +11,24 @@ class TestRun(unittest.TestCase):
     #pylint: disable=unused-argument
     class FakeArgvParser(object):
       def parse(self, argv):
-        return 'options', 'argvunknown'
+        return {'options': 'options'}, ['argvunknown']
     class FakeWorkerFactory(object):
       def produce(self, options, argvunknown):
         return FakeWorker()
     class FakeWorker(object):
       def __call__(self):
-        return 'exitstatus'
+        return 42
     stderr = StringIO.StringIO()
     app = p.Application(FakeArgvParser(), FakeWorkerFactory(), stderr)
-    exitstatus = app.run('argv')
+    exitstatus = app.run(['progname', 'argv'])
     self.assertEqual(stderr.getvalue(), '')
-    self.assertEqual(exitstatus, 'exitstatus')
+    self.assertEqual(exitstatus, 42)
 
   def test_genericerror(self):
     #pylint: disable=unused-argument
     class FakeArgvParser(object):
       def parse(self, argv):
-        return 'options', 'argvunknown'
+        return {'options': 'options'}, ['argvunknown']
     class FakeWorkerFactory(object):
       def produce(self, options, argvunknown):
         return FakeWorker()
@@ -37,6 +37,6 @@ class TestRun(unittest.TestCase):
         raise p.Error('generic error')
     stderr = StringIO.StringIO()
     app = p.Application(FakeArgvParser(), FakeWorkerFactory(), stderr)
-    exitstatus = app.run('argv')
+    exitstatus = app.run(['progname', 'argv'])
     self.assertEqual(stderr.getvalue(), 'generic error\n')
     self.assertEqual(exitstatus, 1)
