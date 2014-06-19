1a271d07ff80c39494bd2efd483762fde5b1b19b
Lesmana Zimmer <lesmana@gmx.de>
2014-06-19 18:27:51 +0200

    remove trivial tests

--- tests/sessionstoreparsertest.py
+++ tests/sessionstoreparsertest.py
@@ -5,46 +5,6 @@ import unittest
 
 import sessionstoreparser as p
 
-class TestGetSessionStore(unittest.TestCase):
-
-  def test_noerror(self):
-    report = []
-    class FakeJsonReader(object):
-      def read(self, filename):
-        report.append(('read', filename))
-        return {'json': 'object'}
-    parser = p.SessionStoreParser(FakeJsonReader(), None, None, None)
-    sessionstore = parser.getsessionstore('filename')
-    self.assertEqual(sessionstore, {'json': 'object'})
-    self.assertEqual(report, [
-          ('read', 'filename')])
-
-class TestGetUrls(unittest.TestCase):
-
-  def test_noerror(self):
-    report = []
-    class FakeUrlGenerator(object):
-      def generate(self, sessionstore):
-        report.append(('generate', sessionstore))
-        return ['urls']
-    parser = p.SessionStoreParser(None, FakeUrlGenerator(), None, None)
-    urls = parser.geturls({'session': 'store'})
-    self.assertEqual(urls, ['urls'])
-    self.assertEqual(report, [
-          ('generate', {'session': 'store'})])
-
-class TestWriteUrls(unittest.TestCase):
-
-  def test_noerror(self):
-    report = []
-    class FakeUrlWriter(object):
-      def write(self, urls):
-        report.append(('write', urls))
-    parser = p.SessionStoreParser(None, None, None, FakeUrlWriter())
-    parser.writeurls(['urls'])
-    self.assertEqual(report, [
-          ('write', ['urls'])])
-
 class TestSessionStoreParse(unittest.TestCase):
 
   def test_noerror(self):
