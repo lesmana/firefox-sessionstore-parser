6681dd2531ca52ac334740b0a24acec39b45f2d5
Lesmana Zimmer <lesmana@gmx.de>
2014-06-24 22:46:24 +0200

    dashes for better readability

--- tests/testdata.js
+++ tests/testdata.js
@@ -6,13 +6,13 @@
           "index": 3,
           "entries": [
             {
-              "url": "http://ow1ot1b2u1/"
+              "url": "http://ow1-ot1-b2u1/"
             },
             {
-              "url": "http://ow1ot1b1u2/"
+              "url": "http://ow1-ot1-b1u2/"
             },
             {
-              "url": "http://ow1ot1s0u3/"
+              "url": "http://ow1-ot1-s0u3/"
             }
           ],
           "attributes": {}
@@ -21,13 +21,13 @@
           "index": 2,
           "entries": [
             {
-              "url": "http://ow1ot2b1u1/"
+              "url": "http://ow1-ot2-b1u1/"
             },
             {
-              "url": "http://ow1ot2s0u2/"
+              "url": "http://ow1-ot2-s0u2/"
             },
             {
-              "url": "http://ow1ot2f1u3/"
+              "url": "http://ow1-ot2-f1u3/"
             }
           ],
           "attributes": {}
@@ -36,13 +36,13 @@
           "index": 1,
           "entries": [
             {
-              "url": "http://ow1ot3s0u1/"
+              "url": "http://ow1-ot3-s0u1/"
             },
             {
-              "url": "http://ow1ot3f1u2/"
+              "url": "http://ow1-ot3-f1u2/"
             },
             {
-              "url": "http://ow1ot3f2u3/"
+              "url": "http://ow1-ot3-f2u3/"
             }
           ],
           "attributes": {}
@@ -55,10 +55,10 @@
             "index": 2,
             "entries": [
               {
-                "url": "http://ow1ct1b1u1/"
+                "url": "http://ow1-ct1-b1u1/"
               },
               {
-                "url": "http://ow1ct1s0u2/"
+                "url": "http://ow1-ct1-s0u2/"
               }
             ],
             "attributes": {}
@@ -70,10 +70,10 @@
             "index": 1,
             "entries": [
               {
-                "url": "http://ow1ct2s0u1/"
+                "url": "http://ow1-ct2-s0u1/"
               },
               {
-                "url": "http://ow1ct2f1u2/"
+                "url": "http://ow1-ct2-f1u2/"
               }
             ],
             "attributes": {}
@@ -88,13 +88,13 @@
           "index": 3,
           "entries": [
             {
-              "url": "http://ow2ot1b2u1/"
+              "url": "http://ow2-ot1-b2u1/"
             },
             {
-              "url": "http://ow2ot1b1u2/"
+              "url": "http://ow2-ot1-b1u2/"
             },
             {
-              "url": "http://ow2ot1s0u3/"
+              "url": "http://ow2-ot1-s0u3/"
             }
           ],
           "attributes": {}
@@ -103,13 +103,13 @@
           "index": 2,
           "entries": [
             {
-              "url": "http://ow2ot2b1u1/"
+              "url": "http://ow2-ot2-b1u1/"
             },
             {
-              "url": "http://ow2ot2s0u2/"
+              "url": "http://ow2-ot2-s0u2/"
             },
             {
-              "url": "http://ow2ot2f1u3/"
+              "url": "http://ow2-ot2-f1u3/"
             }
           ],
           "attributes": {}
@@ -118,13 +118,13 @@
           "index": 1,
           "entries": [
             {
-              "url": "http://ow2ot3s0u1/"
+              "url": "http://ow2-ot3-s0u1/"
             },
             {
-              "url": "http://ow2ot3f1u2/"
+              "url": "http://ow2-ot3-f1u2/"
             },
             {
-              "url": "http://ow2ot3f2u3/"
+              "url": "http://ow2-ot3-f2u3/"
             }
           ],
           "attributes": {}
@@ -137,10 +137,10 @@
             "index": 2,
             "entries": [
               {
-                "url": "http://ow2ct1b1u1/"
+                "url": "http://ow2-ct1-b1u1/"
               },
               {
-                "url": "http://ow2ct1s0u2/"
+                "url": "http://ow2-ct1-s0u2/"
               }
             ],
             "attributes": {}
@@ -152,10 +152,10 @@
             "index": 1,
             "entries": [
               {
-                "url": "http://ow2ct2s0u1/"
+                "url": "http://ow2-ct2-s0u1/"
               },
               {
-                "url": "http://ow2ct2f1u2/"
+                "url": "http://ow2-ct2-f1u2/"
               }
             ],
             "attributes": {}
@@ -170,13 +170,13 @@
           "index": 3,
           "entries": [
             {
-              "url": "http://ow3ot1b2u1/"
+              "url": "http://ow3-ot1-b2u1/"
             },
             {
-              "url": "http://ow3ot1b1u2/"
+              "url": "http://ow3-ot1-b1u2/"
             },
             {
-              "url": "http://ow3ot1s0u3/"
+              "url": "http://ow3-ot1-s0u3/"
             }
           ],
           "attributes": {}
@@ -185,13 +185,13 @@
           "index": 2,
           "entries": [
             {
-              "url": "http://ow3ot2b1u1/"
+              "url": "http://ow3-ot2-b1u1/"
             },
             {
-              "url": "http://ow3ot2s0u2/"
+              "url": "http://ow3-ot2-s0u2/"
             },
             {
-              "url": "http://ow3ot2f1u3/"
+              "url": "http://ow3-ot2-f1u3/"
             }
           ],
           "attributes": {}
@@ -200,13 +200,13 @@
           "index": 1,
           "entries": [
             {
-              "url": "http://ow3ot3s0u1/"
+              "url": "http://ow3-ot3-s0u1/"
             },
             {
-              "url": "http://ow3ot3f1u2/"
+              "url": "http://ow3-ot3-f1u2/"
             },
             {
-              "url": "http://ow3ot3f2u3/"
+              "url": "http://ow3-ot3-f2u3/"
             }
           ],
           "attributes": {}
@@ -219,10 +219,10 @@
             "index": 2,
             "entries": [
               {
-                "url": "http://ow3ct1b1u1/"
+                "url": "http://ow3-ct1-b1u1/"
               },
               {
-                "url": "http://ow3ct1s0u2/"
+                "url": "http://ow3-ct1-s0u2/"
               }
             ],
             "attributes": {}
@@ -234,10 +234,10 @@
             "index": 1,
             "entries": [
               {
-                "url": "http://ow3ct2s0u1/"
+                "url": "http://ow3-ct2-s0u1/"
               },
               {
-                "url": "http://ow3ct2f1u2/"
+                "url": "http://ow3-ct2-f1u2/"
               }
             ],
             "attributes": {}
@@ -254,10 +254,10 @@
           "index": 2,
           "entries": [
             {
-              "url": "http://cw1ot1b1u1/"
+              "url": "http://cw1-ot1-b1u1/"
             },
             {
-              "url": "http://cw1ot1s0u2/"
+              "url": "http://cw1-ot1-s0u2/"
             }
           ],
           "attributes": {}
@@ -266,10 +266,10 @@
           "index": 1,
           "entries": [
             {
-              "url": "http://cw1ot2s0u1/"
+              "url": "http://cw1-ot2-s0u1/"
             },
             {
-              "url": "http://cw1ot2f1u2/"
+              "url": "http://cw1-ot2-f1u2/"
             }
           ],
           "attributes": {}
@@ -282,10 +282,10 @@
             "index": 2,
             "entries": [
               {
-                "url": "http://cw1ct1b1u1/"
+                "url": "http://cw1-ct1-b1u1/"
               },
               {
-                "url": "http://cw1ct1s0u2/"
+                "url": "http://cw1-ct1-s0u2/"
               }
             ],
             "attributes": {}
@@ -297,10 +297,10 @@
             "index": 1,
             "entries": [
               {
-                "url": "http://cw1ct2s0u1/"
+                "url": "http://cw1-ct2-s0u1/"
               },
               {
-                "url": "http://cw1ct2f1u2/"
+                "url": "http://cw1-ct2-f1u2/"
               }
             ],
             "attributes": {}
@@ -316,10 +316,10 @@
           "index": 2,
           "entries": [
             {
-              "url": "http://cw2ot1b1u1/"
+              "url": "http://cw2-ot1-b1u1/"
             },
             {
-              "url": "http://cw2ot1s0u2/"
+              "url": "http://cw2-ot1-s0u2/"
             }
           ],
           "attributes": {}
@@ -328,10 +328,10 @@
           "index": 1,
           "entries": [
             {
-              "url": "http://cw2ot2s0u1/"
+              "url": "http://cw2-ot2-s0u1/"
             },
             {
-              "url": "http://cw2ot2f1u2/"
+              "url": "http://cw2-ot2-f1u2/"
             }
           ],
           "attributes": {}
@@ -344,10 +344,10 @@
             "index": 2,
             "entries": [
               {
-                "url": "http://cw2ct1b1u1/"
+                "url": "http://cw2-ct1-b1u1/"
               },
               {
-                "url": "http://cw2ct1s0u2/"
+                "url": "http://cw2-ct1-s0u2/"
               }
             ],
             "attributes": {}
@@ -359,10 +359,10 @@
             "index": 1,
             "entries": [
               {
-                "url": "http://cw2ct2s0u1/"
+                "url": "http://cw2-ct2-s0u1/"
               },
               {
-                "url": "http://cw2ct2f1u2/"
+                "url": "http://cw2-ct2-f1u2/"
               }
             ],
             "attributes": {}
