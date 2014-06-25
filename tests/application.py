2d391bf23752a1141be729ea444ae0de02b23fda
Lesmana Zimmer <lesmana@gmx.de>
2014-06-25 17:09:17 +0200

    add report to test application

--- tests/application.py
+++ tests/application.py
@@ -8,35 +8,49 @@ import sessionstoreparser as p
 class TestRun(unittest.TestCase):
 
   def test_noerror(self):
-    #pylint: disable=unused-argument
+    report = []
     class FakeArgvParser(object):
       def parse(self, argv):
+        report.append(('parse', argv))
         return {'parsedargv': 'parsedargv'}
     class FakeWorkerFactory(object):
       def produce(self, parsedargv):
+        report.append(('produce', parsedargv))
         return FakeWorker()
     class FakeWorker(object):
       def __call__(self):
+        report.append(('work', ))
         return 42
     stderr = StringIO.StringIO()
     app = p.Application(FakeArgvParser(), FakeWorkerFactory(), stderr)
     exitstatus = app.run(['progname', 'argv'])
     self.assertEqual(stderr.getvalue(), '')
     self.assertEqual(exitstatus, 42)
+    self.assertEqual(report, [
+          ('parse', ['progname', 'argv']),
+          ('produce', {'parsedargv': 'parsedargv'}),
+          ('work', )])
 
   def test_genericerror(self):
-    #pylint: disable=unused-argument
+    report = []
     class FakeArgvParser(object):
       def parse(self, argv):
+        report.append(('parse', argv))
         return {'parsedargv': 'parsedargv'}
     class FakeWorkerFactory(object):
       def produce(self, parsedargv):
+        report.append(('produce', parsedargv))
         return FakeWorker()
     class FakeWorker(object):
       def __call__(self):
+        report.append(('work', ))
         raise p.Error('generic error')
     stderr = StringIO.StringIO()
     app = p.Application(FakeArgvParser(), FakeWorkerFactory(), stderr)
     exitstatus = app.run(['progname', 'argv'])
     self.assertEqual(stderr.getvalue(), 'generic error\n')
     self.assertEqual(exitstatus, 1)
+    self.assertEqual(report, [
+          ('parse', ['progname', 'argv']),
+          ('produce', {'parsedargv': 'parsedargv'}),
+          ('work', )])
