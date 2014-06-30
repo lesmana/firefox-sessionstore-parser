01322860cc46a47801935b4ad3c198f7ad68eb65
Lesmana Zimmer <lesmana@gmx.de>
2014-06-30 11:55:45 +0200

    cosmetic: rename to option

--- sessionstoreparser.py
+++ sessionstoreparser.py
@@ -266,8 +266,8 @@ class UrlFilterFactory(object):
   def getinputs(self, parsedargv):
     inputs = {}
     inputs.update(self.defaultinputs)
-    for whatinputs, template in self.updatetemplate.items():
-      if whatinputs in parsedargv:
+    for option, template in self.updatetemplate.items():
+      if option in parsedargv:
         inputs.update(template)
     for whatinputs in inputs:
       if whatinputs in parsedargv:
